b4df29611bff0439c6ca86102631d6e9512a5130
Johan Olby <johan@olby.se>
2023-07-05 10:14:51 +0200

    Load a silly ULP program and sleep

--- build.rs
+++ build.rs
@@ -1,6 +1,28 @@
+use embuild::*;
+
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
-    embuild::build::LinkArgs::output_propagated("ESP_IDF")?;
+    build::CfgArgs::output_propagated("ESP_IDF")?;
+    build::LinkArgs::output_propagated("ESP_IDF")?;
+    build_ulp()?;
+    Ok(())
+}
+
+fn build_ulp() -> anyhow::Result<()> {
+    use std::iter;
+    use std::{env, path::PathBuf};
+
+    use embuild::utils::OsStrExt;
+
+    let source = path_buf![env::current_dir()?, "src", "wakeup.S"];
+    cargo::track_file(&source);
+    let build_result = espidf::ulp_fsm::Builder::try_from_embuild_env("ESP_IDF", vec![])?.build(
+        iter::once(source.as_path()),
+        PathBuf::from(env::var("OUT_DIR")?).join("ulp_fsm"),
+    )?;
+
+    cargo::set_rustc_env("ULP_FSM_BIN", build_result.bin_file.try_to_str()?);
+    cargo::set_rustc_env("ULP_FSM_RS", build_result.sym_rs_file.try_to_str()?);
+
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,36 @@
-use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+use anyhow::*;
+use esp_idf_hal::{prelude::Peripherals, reset::WakeupReason};
+use esp_idf_sys::{self as _, esp}; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
 
-fn main() {
+mod ulp_code_vars {
+    include!(env!("ULP_FSM_RS"));
+}
+
+const ULP_CODE: &[u8] = include_bytes!(env!("ULP_FSM_BIN"));
+
+fn main() -> Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
+    let wakeup_reason = WakeupReason::get();
+    info!("Wakeup reason: {:?}", wakeup_reason);
+
     info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+
+    let mut ulp_driver = esp_idf_hal::ulp::UlpDriver::new(peripherals.ulp)?;
+
+    info!("Feeling sleepy");
+    unsafe {
+        ulp_driver.load(ULP_CODE)?;
+        ulp_driver.start(ulp_code_vars::wake_up)?;
+
+        esp!(esp_idf_sys::esp_sleep_enable_ulp_wakeup())?;
+        esp_idf_sys::esp_deep_sleep_start();
+    };
 }
